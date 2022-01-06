93c149ac27e102e62970183cc1f0b515744165dd
Shivendra <shivendra.singh@parth.com>
2022-01-06 19:19:45 +0530

    # Testing merge conflict

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+int hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // testing comment
+
         setContentView(R.layout.activity_main);
+        //abc
     }
 }
